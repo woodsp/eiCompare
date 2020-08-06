70edf3fbcfd42678d715481159b4a02c843afdea
Juandalyn Burke <jcburke@uw.edu>
2020-08-06 01:03:33 +0000

    Updated run_geocoder() with parallel processing abilities.

--- R/run_geocoder.R
+++ R/run_geocoder.R
@@ -3,11 +3,12 @@
 #'
 #' @param voter_file A voter file containing the address of the voter.
 #' @param geocoder The type of geocoder used. For US Census API, use "census". For Opencage, use "opencage".
-#' @param parallel A logical variable indicating whether the run will use parallel processing.
+#' @param parallel A logical parameter indicating whether the run will use parallel processing.
 #' @param street_number The location in which the person lives. Ex. 1442 Market Street
 #' @param city The name of the city that the voter lives in.
 #' @param state The state (based on the United States 50 states) that the voter lives in.
 #' @param zipcode The United States Postal Service (USPS) postal code.
+#' @param return Option for creating census geometries such as FIPS codes for block, tract, etc. To turn on this feature use, "geographies" along with assigning values to census_benchmark and census_vintage parameters.
 #' @param census_benchmark The US Census Geocoder API benchmark that references a US Census locator like "Public_AR_Current".
 #' @param census_vintage The US Census Geocoder API vintage that uses the benchmark to determine vintage geography. This can be a name or number.
 #' @param census_output The geocoder output desired can be either: "simple", including only coordinates, or "full including vintage geographies.
@@ -30,6 +31,7 @@ run_geocoder <- function(voter_file,
                          city = "city",
                          state = "GA",
                          zipcode = "residence_zipcode",
+                         census_return = NULL,
                          census_benchmark = "Public_AR_Current",
                          census_vintage = 4,
                          census_output = "single",
@@ -46,10 +48,10 @@ run_geocoder <- function(voter_file,
       .data = voter_file,
       id = voter_file,
       street = street,
-      city = "city",
-      state = "state",
-      zip = "zipcode",
-      return = "geographies",
+      city = city,
+      state = state,
+      zip = zipcode,
+      return = census_return,
       benchmark = census_benchmark,
       vintage = census_vintage,
       output = census_output,
@@ -57,6 +59,62 @@ run_geocoder <- function(voter_file,
     )
 
     voter_file <- cenusus_voter_file
+    return(voter_file)
+  }
+
+  # Runs ONLY if Census Geocoder API is selected, the user is conducting parallel processing and there are more than 10,000 observations/voters.
+  if (geocoder == "census" & parallel == TRUE & num_obs > 10000) {
+    # Set up iterations for parallel processing
+    # Set number of loops needed to batch every 10000 rows of the voter file
+    n_loops <- round(num_obs / 10000, digits = 0) + 1
+    start_row <- 1
+    stop_row <- start_row + 9999
+    last_row_stop <- num_obs
+    last_row_start <- last_row_stop - (num_obs %% 10000) + 1
+
+    # Create empty list that will hold batches of data by 10K observations.
+    dfList <- list()
+
+    # Creates lists of dataframes that holds voter data in batches of 10000
+    for (i in 1:n_loops) {
+      df <- ga_gwin_fulton[start_row:stop_row, ]
+      if (start_row == last_row_start) {
+        df <- ga_gwin_fulton[last_row_start:last_row_stop, ]
+      }
+      dfList <- append(dfList, list(df))
+      start_row <- stop_row + 1
+      stop_row <- start_row + 9999
+    }
+
+    # Run Census Geocoder API
+    clust <- makeCluster(detectCores() - 2)
+    registerDoParallel(clust)
+
+    start_time_2 <- Sys.time()
+
+    census_voter_file <- foreach(i = 1:n_loops, .combine = rbind, .packages = c("censusxy", "sf")) %dopar% {
+      cxy_geocode(
+        .data = dfList[[i]],
+        id = voter_id,
+        street = street,
+        city = city,
+        state = state,
+        zip = zipcode,
+        return = census_return,
+        benchmark = census_benchmark,
+        vintage = census_vintage,
+        output = census_output,
+        class = census_class
+      )
+    }
+
+    (end_time_2 <- Sys.time() - start_time_2)
+
+    stopCluster(clust)
+    gc()
+
+    voter_file <- census_voter_file
+
     return(voter_file)
   }
 
